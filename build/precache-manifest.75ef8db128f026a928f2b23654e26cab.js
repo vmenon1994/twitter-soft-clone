18d7b71026d91615a4a82b4faaffcba7879eec45
Vivek Menon <vivekmenon1994@gmail.com>
2021-09-24 01:37:39 +0530

    build folder

---
+++ build/precache-manifest.75ef8db128f026a928f2b23654e26cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d73a7e839939ab680104901a7399f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaaae8c00ff318c8704d",
+    "url": "/static/css/main.806a55bf.chunk.css"
+  },
+  {
+    "revision": "216a2c0cc5cd14d61f13",
+    "url": "/static/js/2.9a8b548a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a8b548a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaaae8c00ff318c8704d",
+    "url": "/static/js/main.e3c745b2.chunk.js"
+  },
+  {
+    "revision": "c4f7755869c4c476f6c2",
+    "url": "/static/js/runtime-main.6a9201e5.js"
+  }
+]);
